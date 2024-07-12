7071dde9d587acde4db7986fe2abf570c7c8f89c
PrranitArora <116321877+PrranitArora@users.noreply.github.com>
2024-07-12 15:20:46 -0400

    set up basic structure and the necessary pages, there is a button as well to navigate through the pages

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// public/js/script.js
+document.getElementById('prev').addEventListener('click', function() {
+    window.location.href = '/';
+});
+
+document.getElementById('next').addEventListener('click', function() {
+    window.location.href = '/settings';
+});
